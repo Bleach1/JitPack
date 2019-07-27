7cc6ef9450dbb649ca0e00e66424439ca2d187ca
Bleach1 <963262676@qq.com>
2019-07-27 15:24:53 +0800

    BottomNavigation+JetPack Navigation

--- app/src/main/java/mylife/is/it/jitpack/fragment/OneFragment.java
+++ app/src/main/java/mylife/is/it/jitpack/fragment/OneFragment.java
@@ -1,18 +1,25 @@
 package mylife.is.it.jitpack.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.navigation.Navigation;
+
+import java.util.Random;
+
+import mylife.is.it.jitpack.BR;
+import mylife.is.it.jitpack.adapter.CustomBindingAdapter;
+import mylife.is.it.jitpack.base.BaseFragment;
 import mylife.is.it.jitpack.R;
+import mylife.is.it.jitpack.model.BaseObservableGoods;
+import mylife.is.it.jitpack.model.ObservableGoods;
+import mylife.is.it.jitpack.model.OneBean;
 
-public class OneFragment extends Fragment {
+public class OneFragment extends BaseFragment {
 
+
+    private BaseObservableGoods goods;
+    private ObservableGoods goods1;
 
     public static OneFragment newInstance(String param1, String param2) {
         OneFragment fragment = new OneFragment();
@@ -30,19 +37,34 @@ public class OneFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_one, container, false);
+    protected int getLayout() {
+        return R.layout.fragment_one;
     }
 
     @Override
-    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.text).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(view).navigate(R.id.action_oneFragment_to_fourFragment);
-            }
-        });
+    protected void initData() {
+        dataBinding.setVariable(BR.oneUser, new OneBean("LJN", 28, "Bleach"));
+        goods = new BaseObservableGoods("辣条", "嘎嘎辣辣", 20);
+        goods1 = new ObservableGoods("鸡爪子", "四川", 15);
+        dataBinding.setVariable(BR.adapter, new CustomBindingAdapter());
+        dataBinding.setVariable(BR.Goods, goods);
+        dataBinding.setVariable(BR.goodsHandler, new GoodsHandler());
+        dataBinding.setVariable(BR.observableGoods, goods1);
+        dataBinding.setVariable(BR.click, (View.OnClickListener) v -> Navigation.findNavController(v).navigate(R.id.action_oneFragment_to_fourFragment));
     }
+
+    public class GoodsHandler {
+
+        public void changeGoodsName() {
+            goods.setName("code" + new Random().nextInt(100));
+            goods.setPrice(new Random().nextInt(100));
+        }
+
+        public void changeGoodsDetails() {
+            goods.setDetails("hi" + new Random().nextInt(100));
+            goods.setPrice(new Random().nextInt(100));
+        }
+
+    }
+
 }
